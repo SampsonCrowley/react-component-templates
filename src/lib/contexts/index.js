1445049f254126ffa886980ece26bb77f8445c3e
Sampson Crowley <sampsonsprojects@gmail.com>
2018-08-02 20:35:20 -0600

    add context default exports

--- src/lib/contexts/index.js
+++ src/lib/contexts/index.js
@@ -1,12 +1,16 @@
-import ConnectionSpeed from './connection-speed';
-import Menu from './menu';
+import ConnectionSpeedRedux, { ConnectionSpeed } from './connection-speed';
+import MenuRedux, { Menu } from './menu';
 
 export {
+  ConnectionSpeedRedux,
   ConnectionSpeed,
+  MenuRedux,
   Menu
 }
 
 export default {
+  ConnectionSpeedRedux,
   ConnectionSpeed,
+  MenuRedux,
   Menu
 }
