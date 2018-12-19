3769d4359fdf92dfbf0fc454bfef6b538c650e31
Sampson Crowley <sampsonsprojects@gmail.com>
2018-12-19 16:15:09 -0700

    update route parsing data

--- src/lib/helpers/route-parser.js
+++ src/lib/helpers/route-parser.js
@@ -13,6 +13,7 @@ export default class RouteParser {
 
     this.title = null
     this.description = null
+    this.id = null
 
     const root = this.routes.root
 
@@ -35,14 +36,17 @@ export default class RouteParser {
 
     if(this.current.cache && this.routeCache[fullPath]) {
       console.log('cached')
-      this.title = this.routeCache[fullPath]
+      this.title = this.routeCache[fullPath].title
+      this.description = this.routeCache[fullPath].description
+      this.description = this.routeCache[fullPath].description
+      this.id = this.routeCache[fullPath].id
     } else if(this.current.resource) {
       try {
         const regex = new RegExp(this.current.path + (this.current.regex || "/(.*?)(\\/|\\?|$)")),
               matches = location.pathname.match(regex);
 
         if(matches) {
-          let id = matches[this.current.match_idx || (this.current.path ? 1 : 0)];
+          let description, id = matches[this.current.match_idx || (this.current.path ? 1 : 0)]
           if(this.current.api && id){
             if((`${id}`.toLowerCase() === 'new')) {
               id = 'New'
@@ -50,16 +54,18 @@ export default class RouteParser {
               const result = await fetch(this.current.api + id),
                     resource = await result.json()
               id = resource[this.current.method || 'title']
+              description = !!this.current.direct_description && resource[this.current.description_method || 'description']
             }
           }
-          id = id || 'Index'
-          this.title = this.current.title.replace(/%RESOURCE%/, id)
-          this.description = this.current.description.replace(/%RESOURCE%/, id)
+          this.id = id || 'Index'
+          this.title = this.current.title.replace(/%RESOURCE%/, this.id)
+          this.description = description || (this.current.description || '').replace(/%RESOURCE%/, this.id)
 
           if(this.current.cache) this.routeCache[fullPath] = this.title
         } else {
           this.title = this.current.index_title || this.current.title.replace(/%RESOURCE%/, 'Index')
           this.description = this.current.index_description || this.current.description.replace(/%RESOURCE%/, 'Index')
+          this.id = 'Index'
         }
 
       } catch (e) {
@@ -69,14 +75,17 @@ export default class RouteParser {
       }
     }
 
+    this.id = this.id || ''
     this.title = this.title || this.current.title
     this.description = this.description || this.current.description
 
     this.setDocumentTitle()
 
     return {
+      id: this.id,
       title: this.title,
-      subtitle: this.description,
+      description: this.description,
+      currentRoute: this.current
     }
   }
 
